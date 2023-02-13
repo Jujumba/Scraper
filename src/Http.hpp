d69f46855e6bddbe1554d49e298cba8ad2027b1a
Jujumba <far77578@gmail.com>
2023-02-13 22:14:18 +0100

    Http: make methods static

--- src/Http.hpp
+++ src/Http.hpp
@@ -4,20 +4,23 @@
 #define BUFFER_SIZE 1024
 #define HTTP_PORT 80
 #define REQUIRED_VERSION 0x101
+#define HTTP_DELIM1 "\r\n\r\n"
+#define HTTP_DELIM2 "\n\r\n\r"
+#define HTTP_PREFIX "http"
 #pragma comment(lib, "ws2_32.lib")
-#include <Windows.h>
+#include <windows.h>
 #include <string>
 #include <fstream>
 
 typedef unsigned long ulong;
 class Http {
 public:
-    std::string get(const std::string&, bool = false);
+    static std::string get(const std::string&, bool = false);
 private:
-    void connect(SOCKET*, std::string&);
-    std::string read(std::string&);
-    void parse_url(std::string&, std::string&, std::string&, std::string&);
-    ulong get_header_length(const std::string&);
+    static void connect(SOCKET*, std::string&);
+    static std::string read(std::string&);
+    static void parse_url(std::string&, std::string&, std::string&);
+    static ulong get_header_length(const std::string&);
 };
 
 
