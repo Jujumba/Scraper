8b18c026a058c32c68add584b5b74ffe4e24a93f
Jujumba <far77578@gmail.com>
2023-02-15 22:49:30 +0100

    Scraper: allow to parse https url

--- src/Scraper.cpp
+++ src/Scraper.cpp
@@ -1,13 +1,13 @@
 #include "Scraper.hpp"
 void scraper::Scraper::scrap(const std::string& root) {
-    std::string content = client.get(root, true);
+    std::string content = client.get(root);
     ulong start = 0, end = 0;
     if (std::find(urls.begin(), urls.end(), root) != urls.end()) return;
     while (end < content.size()) {
         start = content.find('\"', end);
         end = content.find('\"', start + 1);
         const std::string& url = content.substr(start + 1, end - start - 1);
-        if (std::count(urls.begin(), urls.end(), url) == 0 && (url.starts_with(HTTP_PREFIX))) {
+        if (std::count(urls.begin(), urls.end(), url) == 0 && (url.starts_with(HTTP_PREFIX) || url.starts_with(HTTPS_PREFIX))) {
             urls.push_back(url);
             std::cout << url << std::endl;
             scrap(url);
